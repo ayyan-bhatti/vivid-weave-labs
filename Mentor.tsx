b4cab7665e43b6fc76ee7280aca594e83148f976
ayyan-bhatti <147022373+ayyan-bhatti@users.noreply.github.com>
2026-04-05 18:45:19 +0500

    Rename Arsalan.tsx to Mentor.tsx and update page header

---
+++ Mentor.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Mentor = () => {
+    return (
+        <div>
+            <h1>Mentor</h1>
+            {/* Other content goes here */}
+        </div>
+    );
+};
+
+export default Mentor;
